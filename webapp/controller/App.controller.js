3efc0290a1517764830d6011886cdee4c4808e2c
MustafaTahirAkdemir <themtahir@gmail.com>
2024-10-06 07:35:53 +0300

    Component Configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,40 +1,21 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], (Controller, MessageToast, JSONModel, ResourceModel) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
     "use strict";
 
-    return Controller.extend("ui5.walkthrough.controller.App",{
-        onInit() {
-            // set data model on view
-            const oData = {
-               recipient : {
-                  name : "World"
-               }
-            };
-            const oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
+    return Controller.extend("ui5.walkthrough.controller.App", {
+      onShowHello() {
+        // alert("Hello world")
+        // MessageToast.show("Hello World");
 
-            const i18nModel = new ResourceModel({
-                bundleName: "ui5.walkthrough.i18n.i18n"
-             });
-             this.getView().setModel(i18nModel, "i18n");
-         },
-         
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-
-
-        onShowHello(){
-            // alert("Hello world")
-            // MessageToast.show("Hello World");
-
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            MessageToast.show(sMsg);
-        }
+        MessageToast.show(sMsg);
+      },
     });
-});
+  }
+);
