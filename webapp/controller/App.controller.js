7a2fa21cab841ee93f40d3b1361b93de615d6e8d
MustafaTahirAkdemir <themtahir@gmail.com>
2024-10-06 08:15:42 +0300

    Custom CSS and Theme Colors

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,18 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  (Controller, MessageToast) => {
-    "use strict";
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], (Controller, MessageToast) => {
+	"use strict";
 
-    return Controller.extend("ui5.walkthrough.controller.App", {
-      onShowHello() {
-        // alert("Hello world")
-        // MessageToast.show("Hello World");
+	return Controller.extend("ui5.walkthrough.controller.App", {
+		onShowHello() {
+			// read msg from i18n model
+			const oBundle = this.getView().getModel("i18n").getResourceBundle();
+			const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-        MessageToast.show(sMsg);
-      },
-    });
-  }
-);
+			// show message
+			MessageToast.show(sMsg);
+		}
+	});
+});
